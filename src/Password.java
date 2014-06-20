e3554c0a80c47edc8e8ec719f6ca301c60fafcab
mahmoud hanafy <mahmooud.hanafy@gmail.com>
2014-06-20 10:30:34 +0300

    Added User Class Implementation

--- src/Password.java
+++ src/Password.java
@@ -3,6 +3,10 @@ import java.io.Serializable;
 
 public class Password implements Serializable {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5074126984308457819L;
 	private byte[] ePassword ;
 	
 	
