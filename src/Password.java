1fac1e0ddcd112b8924dcbb72470dabbd507d5b8
zonkoly <mai@ubuntu.ubuntu-domain>
2014-06-20 18:24:35 +0200

    GCM added

--- src/Password.java
+++ src/Password.java
@@ -3,12 +3,15 @@ import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
+import java.security.SecureRandom;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.KeyGenerator;
 import javax.crypto.NoSuchPaddingException;
-import javax.crypto.spec.GCMParameterSpec;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
 public class Password implements Serializable {
@@ -18,9 +21,9 @@ public class Password implements Serializable {
 	 */
 	private static final long serialVersionUID = -5074126984308457819L;
 	private static final int length = 32;
-	private byte[] ePassword;
+	private byte[] ePassword, ivBytes;
+	private SecretKeySpec key ;
 	private static final byte[] special = "$".getBytes();
-	private byte[] key="91945D3F4DCBEE0BF45EF52255F095A4".getBytes(), src="BECAF043B0A23D843194BA972C66DEBD".getBytes(), A="FA3BFD4806EB53FA".getBytes();
 	public Password(byte[] pass, boolean encrypted) throws InvalidKeyException,
 			NoSuchAlgorithmException, NoSuchProviderException,
 			NoSuchPaddingException, InvalidAlgorithmParameterException,
@@ -50,15 +53,19 @@ public class Password implements Serializable {
 			InvalidKeyException, InvalidAlgorithmParameterException,
 			IllegalBlockSizeException, BadPaddingException {
 
-		Cipher cipher = Cipher.getInstance("AES/EAX/NoPadding", "BC");
-		SecretKeySpec key = new SecretKeySpec(this.key, "AES");
+		KeyGenerator kg = KeyGenerator.getInstance("AES");
+		kg.init(128);
+		SecretKey sk = kg.generateKey();
+		key = new SecretKeySpec(sk.getEncoded(), "AES");
 
-		// GCMParameterSpec mapped to AEADParameters and overrides default MAC
-		// size
-		GCMParameterSpec spec = new GCMParameterSpec(128, this.src);
-		cipher.init(Cipher.ENCRYPT_MODE, key, spec);
+		// generate random Initial Vector with length 15
+		SecureRandom random = new SecureRandom();
+		 ivBytes = new byte[16];
+		random.nextBytes(ivBytes);
+		IvParameterSpec ivSpec = new IvParameterSpec(ivBytes);	
+		Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding", "BC");
 
-		cipher.updateAAD(this.A);
+		cipher.init(Cipher.ENCRYPT_MODE, key, ivSpec);
 		byte[] cipherText = cipher.doFinal(plainText);
 		return cipherText;
 	}
@@ -68,15 +75,9 @@ public class Password implements Serializable {
 			InvalidKeyException, InvalidAlgorithmParameterException,
 			IllegalBlockSizeException, BadPaddingException {
 
-		Cipher cipher = Cipher.getInstance("AES/EAX/NoPadding", "BC");
-		SecretKeySpec key = new SecretKeySpec(this.key, "AES");
-
-		// GCMParameterSpec mapped to AEADParameters and overrides default MAC
-		// size
-		GCMParameterSpec spec = new GCMParameterSpec(128, this.src);
-		cipher.init(Cipher.DECRYPT_MODE, key, spec);
-
-		cipher.updateAAD(this.A);
+		Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding", "BC");
+		IvParameterSpec ivSpec = new IvParameterSpec(ivBytes);
+		cipher.init(Cipher.DECRYPT_MODE, key, ivSpec);
 		byte[] plainText = cipher.doFinal(ePassword);
 		return plainText;
 	}
@@ -91,10 +92,14 @@ public class Password implements Serializable {
 	}
 	
 	public static void main(String[] args) throws InvalidKeyException, NoSuchAlgorithmException, NoSuchProviderException, NoSuchPaddingException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException {
-		Password pass = new Password("123756".getBytes(), false);
-		System.out.println(new String(pass.getPass()).length());
-		System.out.println(new String(special));
-		System.out.println(special.length);
+		for(int i=0;i<1000;i+=100)
+		{
+			String password = "Password is "+i;
+			Password pass = new Password(password.getBytes(), false);
+			System.out.println("**************************************************");
+			System.out.println("Original  = "+password);
+			System.out.println("Decrypted = "+new String(pass.decrypte()));
+		}
 	}
 
 }
