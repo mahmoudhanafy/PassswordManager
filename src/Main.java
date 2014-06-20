97921f4da869300727e0cc65cc15623f48ee7a0a
Zonkoly <m.zonkoly@gmail.com>
2014-06-20 10:35:33 +0300

    Added User Class Implementation

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,3 @@
-import java.security.NoSuchAlgorithmException;
-import java.security.spec.KeySpec;
-
-import javax.crypto.Cipher;
-import javax.crypto.SecretKeyFactory;
-import javax.crypto.spec.GCMParameterSpec;
-
-
 public class Main {
 
-	
-	public static void main(String[] args) throws NoSuchAlgorithmException {
-	}
 }
